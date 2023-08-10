619cc0783dafaf52bcfbd4c0f8e3c2ac7a651180
arturconrado <conraderp@gmail.com>
2023-08-10 13:06:11 -0300

    new formate data

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+
 from fastapi import FastAPI
 from sqlalchemy import create_engine, text
 import pandas as pd
@@ -7,104 +9,124 @@ stats = FastAPI()
 
 DATABASE_URL = "postgresql://artur:Aqwe123!@localhost:5432/lotofacil"
 engine = create_engine(DATABASE_URL)
+
+# Funções utilitárias
 is_prime = utils.is_prime
 is_fibonacci = utils.is_fibonacci
 
 
-@stats.on_event("startup")
-def load_data():
-    global data
-
-    # Conexão com o banco de dados
+# Função para carregar dados
+def load_data_from_db():
     try:
-        engine = create_engine(DATABASE_URL)
-        print("Conexão com o banco de dados estabelecida.")
-    except Exception as e:
-        print("Erro ao conectar ao banco de dados:", e)
-        return
-
-    # Executando a consulta SQL
-    try:
-        data = pd.read_sql('SELECT * FROM resultados', engine)
-        print("Consulta SQL executada com sucesso. Número de linhas:", len(data))
+        return pd.read_sql('SELECT * FROM resultados', engine)
     except Exception as e:
         print("Erro ao executar a consulta SQL:", e)
-        return
+        return pd.DataFrame()
 
+
+# Função para calcular estatísticas
+def calculate_statistics(data):
+    logging.info("Calculando estatísticas...")
     # Verificando se o DataFrame está vazio
     if data.empty:
         print("DataFrame está vazio. Verifique a tabela 'resultados' no banco de dados.")
-        return
+        return {}
 
-    # Cálculo da contagem de números primos
-    try:
-        prime_counts = data.iloc[:, 2:-1].apply(lambda row: sum(is_prime(n) for n in row), axis=1)
-        print("Prime counts calculado com sucesso.")
-        data['prime_count'] = prime_counts
-    except Exception as e:
-        print("Erro ao calcular a contagem de números primos:", e)
-        return
-
-    data = pd.read_sql('SELECT * FROM resultados', engine)
-    print(data.columns)
+    # Cálculo das estatísticas
     data['fibonacci_count'] = data.iloc[:, 2:-1].apply(lambda row: sum(is_fibonacci(n) for n in row), axis=1)
     data['even_count'] = data.iloc[:, 2:-1].apply(lambda row: sum(n % 2 == 0 for n in row), axis=1)
     data['odd_count'] = data.iloc[:, 2:-1].apply(lambda row: sum(n % 2 == 1 for n in row), axis=1)
-    prime_counts = data.iloc[:, 2:-1].apply(lambda row: sum(is_prime(n) for n in row), axis=1)
-    print("Prime counts:", prime_counts)
-    data['prime_count'] = prime_counts
-
-
-@stats.get("/stats")
-def get_stats():
-    print("Iniciando o carregamento dos dados...")
-    load_data()
-    mean_sum = data['Soma'].mean().item()
-    max_sum = data['Soma'].max().item()
-    min_sum = data['Soma'].min().item()
-    average_even_numbers = data['even_count'].mean().item()
-    average_odd_numbers = data['odd_count'].mean().item()
-    average_prime_numbers = data['prime_count'].mean().item()
-    average_fibonacci_numbers = data['fibonacci_count'].mean().item()
-    last_sum = data['Soma'].iloc[-1].item()
-    max_even_count = data['even_count'].max().item()
-    max_odd_numbers = data['odd_count'].max().item()
-    max_prime_count = data['prime_count'].max().item()
-    max_fibonacci_count = data['fibonacci_count'].max().item()
-    std_dev_sum = data['Soma'].std().item()
+    data['prime_count'] = data.iloc[:, 2:-1].apply(lambda row: sum(is_prime(n) for n in row), axis=1)
+    data['Soma'] = data.iloc[:, 2:-1].sum(axis=1)
 
     # Resultado das estatísticas
     result = {
-        "average_sum": mean_sum,
-        "max_sum": max_sum,
-        "min_sum": min_sum,
-        "std_dev_sum": std_dev_sum,
-        "last_sum": last_sum,
-        "average_even_numbers": average_even_numbers,
-        "average_odd_numbers": average_odd_numbers,
-        "average_prime_numbers": average_prime_numbers,
-        "max_even_count": max_even_count,
-        "max_odd_numbers": max_odd_numbers,
-        "max_prime_count": max_prime_count,
-        "average_fibonacci_numbers": average_fibonacci_numbers,
-        "max_fibonacci_count": max_fibonacci_count,
+        'average_sum': data['Soma'].mean(),
+        'max_sum': data['Soma'].max(),
+        'min_sum': data['Soma'].min(),
+        'last_sum': data['Soma'].iloc[-1],
+        'sum_std': data['Soma'].std(),
+        'max_odd_count': data['odd_count'].max(),
+        'max_even_count': data['even_count'].max(),
+        'max_prime_count': data['prime_count'].max(),
+        'max_fibonacci_count': data['fibonacci_count'].max(),
+        'last_odd_count': data['odd_count'].iloc[-1],
+        'last_even_count': data['even_count'].iloc[-1],
+        'last_prime_count': data['prime_count'].iloc[-1],
+        'last_fibonacci_count': data['fibonacci_count'].iloc[-1],
+        'fibonacci_count_std': data['fibonacci_count'].std(),
+        'prime_count_std': data['prime_count'].std(),
+        'even_count_std': data['even_count'].std(),
+        'odd_count_std': data['odd_count'].std(),
+        'average_odd_count': data['odd_count'].mean(),
+        'average_even_count': data['even_count'].mean(),
+        'average_prime_count': data['prime_count'].mean(),
+        'average_fibonacci_count': data['fibonacci_count'].mean(),
+        'min_odd_count': data['odd_count'].min(),
+        'min_even_count': data['even_count'].min(),
+        'min_prime_count': data['prime_count'].min(),
+        'min_fibonacci_count': data['fibonacci_count'].min(),
+        'last_bola1': data['bola 1'].iloc[-1],
+        'last_bola2': data['bola 2'].iloc[-1],
+        'last_bola3': data['bola 3'].iloc[-1],
+        'last_bola13': data['bola 13'].iloc[-1],
+        'last_bola14': data['bola 14'].iloc[-1],
+        'last_bola15': data['bola 15'].iloc[-1],
+        'average_bola1': data['bola 1'].mean(),
+        'average_bola2': data['bola 2'].mean(),
+        'average_bola3': data['bola 3'].mean(),
+        'average_bola13': data['bola 13'].mean(),
+        'average_bola14': data['bola 14'].mean(),
+        'average_bola15': data['bola 15'].mean(),
+        'max_bola1': data['bola 1'].max(),
+        'max_bola2': data['bola 2'].max(),
+        'max_bola3': data['bola 3'].max(),
+        'max_bola13': data['bola 13'].max(),
+        'max_bola14': data['bola 14'].max(),
+        'max_bola15': data['bola 15'].max(),
+        'min_bola13': data['bola 13'].min(),
+        'min_bola14': data['bola 14'].min(),
+        'min_bola15': data['bola 15'].min(),
+        'bola13_std': data['bola 13'].std(),
+        'bola14_std': data['bola 14'].std(),
+        'bola15_std': data['bola 15'].std(),
+
     }
 
+    # Convertendo os valores para tipos nativos do Python
+    result = {key: value.item() if hasattr(value, 'item') else value for key, value in result.items()}
+
+    return result
+
+
+# Função para inserir estatísticas no banco de dados
+def insert_statistics(result):
+    logging.info("Inserindo estatísticas no banco de dados...")
+    fields = ', '.join(result.keys())
+    values = ', '.join([f':{key}' for key in result.keys()])
+    query_text = f"""
+        INSERT INTO stats (
+            {fields}
+        ) VALUES (
+            {values}
+        )
+    """
+    query = text(query_text)
+    logging.info("Query de inserção de estatísticas:")
+
     with engine.connect() as conn:
-        query = text("""
-            INSERT INTO stats (
-                average_sum, max_sum, min_sum, std_dev_sum, last_sum,
-                average_even_numbers, average_odd_numbers, average_prime_numbers,
-                max_even_count, max_odd_numbers, max_prime_count,
-                average_fibonacci_numbers, max_fibonacci_count
-            ) VALUES (
-                :average_sum, :max_sum, :min_sum, :std_dev_sum, :last_sum,
-                :average_even_numbers, :average_odd_numbers, :average_prime_numbers,
-                :max_even_count, :max_odd_numbers, :max_prime_count,
-                :average_fibonacci_numbers, :max_fibonacci_count
-            )
-            """)
         conn.execute(query, result)
         conn.commit()
+
+
+# Rota de inicialização para carregar dados e calcular estatísticas
+@stats.on_event("startup")
+@stats.get("/stats")
+def load_and_calculate():
+    logging.info("Iniciando o cálculo das estatísticas...")
+    data = load_data_from_db()
+    result = calculate_statistics(data)
+    insert_statistics(result)
+    logging.info("Estatísticas calculadas e inseridas no banco de dados.")
 
     return result
--- quente-frio.py
+++ quente-frio.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from sqlalchemy import create_engine
 import pandas as pd
+from statsmodels.tsa.arima_model import ARIMA
+
 
 quente_frio = FastAPI()
 
@@ -15,11 +17,14 @@ def hot_and_cold_numbers():
     data = pd.read_sql('SELECT * FROM resultados', engine)
     bola_columns = [col for col in data.columns if 'bola' in col.lower()]
 
+    # Treine o modelo ARIMA
+    model = ARIMA(data, order=(5, 1, 0))
+    model_fit = model.fit(disp=0)
     # Calcular a frequência de cada número
     frequency = data[bola_columns].value_counts()
 
     # Identificar os números quentes e frios
-    hot_numbers = frequency.head(5).index.tolist()
+    hot_numbers_prediction = model_fit.forecast(steps=5)
     cold_numbers = frequency.tail(5).index.tolist()
 
-    return {"hot_numbers": hot_numbers, "cold_numbers": cold_numbers}
+    return {"hot_numbers": hot_numbers_prediction, "cold_numbers": cold_numbers}
